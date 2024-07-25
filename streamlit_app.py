399590c979de12ffe8c958024bd9c23e5a367477
Isaiah Gonzales <110946128+Isaiah-Gonzales@users.noreply.github.com>
2024-07-25 16:44:04 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -59,7 +59,7 @@ if st.button("Run my simulation"):
     i = 0
     if distribution == "poor":
       while i < numLoops:
-        results.append(blender2D(thiefSize=thiefSize, 
+        results.append(np.mean(blender2D(thiefSize=thiefSize, 
                   percentPurityOfDS=percentPurityOfDS,
                   DL=DL, 
                   blenderSize=blenderSize, 
@@ -67,17 +67,17 @@ if st.button("Run my simulation"):
                   distribution= distribution,
                   numClumps=numClumps,
                   sizeClumps=sizeClumps,
-                  verbose=False))
+                  verbose=False)))
         i += 1
     else:
       while i < numLoops:
-        results.append(blender2D(thiefSize=thiefSize, 
+        results.append(np.mean(blender2D(thiefSize=thiefSize, 
                   percentPurityOfDS=percentPurityOfDS,
                   DL=DL, 
                   blenderSize=blenderSize, 
                   fillRatio=fillRatio,
                   distribution= distribution,
-                  verbose=False))
+                  verbose=False)))
         i += 1
     st.write("**Min Assay Observed** = " + str(min(results)))
     st.write("**Max Assay Observed** = " +str(max(results)))
