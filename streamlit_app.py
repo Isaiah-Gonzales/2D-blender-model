764efda6b3e4d5555e7b72c78f9c978ab27a9dce
Isaiah Gonzales <110946128+Isaiah-Gonzales@users.noreply.github.com>
2024-07-25 15:34:19 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
 from model import *
 import streamlit as st
-
 st.title("BU Sampler tool")
+st.write("Hi and welcome, this tool creates a model of a blender depending on your inputs. It's recommended to start with a single run to get an understanding of how the program works. If you have any feedback, please reach out to either Isaiah Gonzales or Rajarshi Sengupta.")
