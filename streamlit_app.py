e6f428e4bde455e58fd569cec86da543c31315b5
Isaiah Gonzales <110946128+Isaiah-Gonzales@users.noreply.github.com>
2024-07-25 17:06:39 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,26 +7,25 @@ model_type = st.selectbox("Would you like to perform a singe run or multiple run
 
 if model_type == "single run":
   thiefSize = st.number_input("Size of sample thief (mL)", min_value = 1, max_value = 100)
-  percentPurityOfDS = st.number_input("Purity of DS (%)", min_value= 0, max_value = 110)
-  DL = st.number_input("Blend drug load (%)", min_value = 0, max_value = 100)
+  percentPurityOfDS = st.number_input("Purity of DS (%)", min_value= 0, max_value = 110, value = 100)
+  DL = st.number_input("Blend drug load (%)", min_value = 0, max_value = 100, value = 20)
   blenderSize = st.number_input("Size of blender (mL)", min_value=5)
-  fillRatio = st.number_input("Fill volume (%)", min_value=10, help="What percentage of the blenders total volume is filled with powder?")
+  fillRatio = st.number_input("Fill volume (%)", min_value=10,value=10, help="What percentage of the blenders total volume is filled with powder?")
   distribution = st.selectbox("Please choose how you wish the powder to be distributed in the blender", ["unmixed", "random", "uniform", "poor"])
   if distribution == "poor":
-    numClumps = st.number_input("Number of DS clumps present", min_value = 1, help ="This will create n number of DS clump in the blender, increasing this value makes the distribution more poor")
-    sizeClumps = st.number_input("Size of DS clumps", min_value = 2, help="This will control how large the clumps of DS are, increasing this value will make the distribution more poor")
+    numClumps = st.number_input("Number of DS clumps present", min_value = 1,value=100, help ="This will create n number of DS clump in the blender, increasing this value makes the distribution more poor")
+    sizeClumps = st.number_input("Size of DS clumps", min_value = 2,value=1000, help="This will control how large the clumps of DS are, increasing this value will make the distribution more poor")
     
 if model_type == "multiple runs":
   numLoops = int(st.number_input("How many simulations would you like to perform and average", min_value = 1, max_value = 500))
   thiefSize = st.number_input("Size of sample thief (mL)", min_value = 1, max_value = 100)
-  percentPurityOfDS = st.number_input("Purity of DS (%)", min_value= 0, max_value = 110)
-  DL = st.number_input("Blend drug load (%)", min_value = 0, max_value = 100)
+  percentPurityOfDS = st.number_input("Purity of DS (%)", min_value= 0, max_value = 110, value = 100)  DL = st.number_input("Blend drug load (%)", min_value = 0, max_value = 100)
   blenderSize = st.number_input("Size of blender (mL)", min_value=5)
-  fillRatio = st.number_input("Fill volume (%)", min_value=10, help="What percentage of the blenders total volume is filled with powder?")
+  fillRatio = st.number_input("Fill volume (%)", min_value=10,value=10, help="What percentage of the blenders total volume is filled with powder?")
   distribution = st.selectbox("Please choose how you wish the powder to be distributed in the blender", ["unmixed", "random", "uniform", "poor"])
   if distribution == "poor":
-    numClumps = st.number_input("Number of DS clumps present", min_value = 1, help ="This will create n number of DS clump in the blender, increasing this value makes the distribution more poor")
-    sizeClumps = st.number_input("Size of DS clumps", min_value = 2, help="This will control how large the clumps of DS are, increasing this value will make the distribution more poor")
+    numClumps = st.number_input("Number of DS clumps present", min_value = 1,value=100, help ="This will create n number of DS clump in the blender, increasing this value makes the distribution more poor")
+    sizeClumps = st.number_input("Size of DS clumps", min_value = 2,value=1000, help="This will control how large the clumps of DS are, increasing this value will make the distribution more poor")
     
 if st.button("Run my simulation"):
   if model_type == "single run":
