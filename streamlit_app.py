fdccb2421262fca98c666c1c1d6c799f769b3ae6
Isaiah Gonzales <110946128+Isaiah-Gonzales@users.noreply.github.com>
2024-08-07 14:52:15 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 from model import *
 import streamlit as st
 st.title("BU Sampler tool")
-st.write("Hi and welcome, this tool creates a model of a blender depending on your inputs. It's recommended to start with a single run to get an understanding of how the program works. If you have any feedback, please reach out to either <u>Isaiah Gonzales</u> or <u>Rajarshi Sengupta</u>.")
+st.write("Hi and welcome, this tool creates a model of a blender depending on your inputs. It's recommended to start with a single run to get an understanding of how the program works. If you have any feedback, please reach out to either **Isaiah Gonzales** or **Rajarshi Sengupta**.")
 
 st.sidebar.write("**Input simulation parameters here**")
 model_type = st.sidebar.selectbox("Would you like to perform a singe run or multiple runs?", ["-","single run","multiple runs"], help ="**Single:** This will simulate one blender and sample top, middle, bottom positions, then return assay values for those positions. **Multiple:** This will perform a specified number of simulations and return the distribution of assay values for each position.") 
